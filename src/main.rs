9a1caa1471a72b56be24a949b4a3950d46fb690b
Julian Ospald <hasufell@posteo.de>
2020-01-26 01:45:39 +0100

    Allow to configure numbers next to icons
    This also introduces cli parsing and a global mutable
    Settings mutex.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 use std::env;
+use settings::*;
+use std::str::FromStr;
+use std::process::exit;
+
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate clap;
 
 mod branch;
 mod changes;
+mod settings;
+
 
 fn main() -> std::io::Result<()> {
+    let mut app = clap_app!(rustygitprompt =>
+        (version: "1.0")
+        (author: "Mikołaj Baranowski <mikolajb@gmail.com>")
+        (about: "A simple git prompt in rust")
+        (@arg FORMAT: -i --icon_list_format +takes_value "Sets the format for icon list: superscript, subscript or digits (default)")
+    );
+
+    let format = app.clone().get_matches().value_of("FORMAT").map(|s| s.to_string());
+
+    if let Some(c) = format {
+        if let Ok(f) = IconListFormat::from_str(&c) {
+            let mut settings = SETTINGS.lock().unwrap();
+            settings.icon_list_format = f;
+        } else {
+            let _ = app.print_help();
+            println!("");
+            exit(1);
+        }
+    };
+
     let repo = match git2::Repository::discover(env::current_dir()?.as_path()) {
         Ok(r) => r,
         Err(_r) => return Ok(()),
